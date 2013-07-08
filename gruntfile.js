57df7b27f78908a29bbcfbd00f53fa55f0d3d051
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-08 22:42:19 +0200

    First draft of gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,37 @@
 module.exports = function(grunt) {
-  
+
+  var sourceOrigin = 'src/**/*.js';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [sourceOrigin],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        report: 'gzip'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', sourceOrigin],
+      options: grunt.file.readJSON('.jshintrc')
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('test', 'Lints and unit tests', ['jshint']);
+  grunt.registerTask('default', 'Default task', ['test', 'concat', 'uglify']);
+
 };
