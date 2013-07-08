8c13558f45642da538b0917f507be8a0c2f2f992
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-08 22:37:18 +0200

    Stub texture file

---
+++ src/texture.js
@@ -0,0 +1,20 @@
+(function(root) {
+  
+  var Texture = (function() {
+    
+    var Texture = function() {};
+    
+    return Texture;
+    
+  })();
+  
+  if (typeof root.define === 'function') {
+    define('imp/texture', function() {
+      return Texture;
+    })
+  } else {
+    root.Imp = root.Imp || {};
+    root.Imp.Texture = Texture;
+  }
+  
+})(this);
