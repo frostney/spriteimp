60822481bc0b68a44a5d2a03ad5f3183272d36e1
Johannes Stein <johannesstein@freeze-dev.com>
2013-09-02 22:43:49 +0200

    Updated dist JS

--- dist/spriteimp.js
+++ dist/spriteimp.js
@@ -1,15 +1,21 @@
 (function(root) {
   
-  root.udefine.globals.root = root;
   root.udefine.globals.jquery = root.jQuery;
-  root.udefine.globals['imp/sprite'] = (function() {
-    root.Imp = root.Imp || {};
-    return root.Imp.Sprite;
-  })();
-  root.udefine.globals['imp/texture'] = (function() {
-    root.Imp = root.Imp || {};
-    return root.Imp.Texture;
-  })();
+  
+  root.udefine.globals['imp/sprite'] = undefined;
+  root.udefine.globals['imp/texture'] = undefined;
+  
+  var impObj = root.Imp = root.Imp || {};
+  
+  root.udefine.inject['imp/sprite'] = {
+    root: impObj,
+    name: 'Sprite'
+  };
+  
+  root.udefine.inject['imp/texture'] = {
+    root: impObj,
+    name: 'Texture'
+  };
   
 })(this);
 
